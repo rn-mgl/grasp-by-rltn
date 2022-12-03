612706fb634acd369eacefe27238add84ed9551a
rn-mgl <reinb2ra@gmail.com>
2022-12-03 14:18:12 +0800

    url

--- src/context.js
+++ src/context.js
@@ -3,7 +3,7 @@ import React from "react";
 const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
-  const url = "http://192.168.1.121:9000";
+  const url = "https://grasp-server.onrender.com";
 
   return <AppContext.Provider value={{ url }}>{children}</AppContext.Provider>;
 };
